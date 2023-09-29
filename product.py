4e458308702f0bde4e15010cc1be5ee622c92d4b
ygladzik <ygladzik@gmail.com>
2023-09-29 11:25:11 -0300

    crie classe Product

---
+++ product.py
@@ -0,0 +1,8 @@
+class Product:
+
+    def __init__(self, name, description, date_fabrication, is_active):
+        self._name = name
+        self._description = description
+        self._date_fabrication = date_fabrication
+        self._is_active = is_active
+        
