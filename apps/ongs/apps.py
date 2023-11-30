636f243108202336b81e0319d73eb4330cba72d7
ygladzik <ygladzik@gmail.com>
2023-11-30 14:41:21 -0300

    aaaaaaaaa

--- apps/ongs/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class OngsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ongs'
-    verbose_name = 'Ongs'
-
--- apps/ongs/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Ong
-
-class OngForm(forms.ModelForm):
-
-    class Meta:
-        model = Ong
-        exclude = ('created_on' , 'updated_on',)
